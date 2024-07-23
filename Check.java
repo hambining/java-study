d93a75319baef0e2cf176db66252a9a75ad505de
hyunbini2942@gmail.com <hambining@HamBook.local>
2024-07-23 17:32:06 +0900

    인덱스 수정

--- Check.java
+++ Check.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 
 public class Check {
     Result result = new Result();
-
     public void count(ArrayList<Integer> comNumList, ArrayList<Integer> userNumList) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
