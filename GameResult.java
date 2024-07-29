1a06922fcef8bc8d0f276c5c115369b590576f5b
hyunbini2942@gmail.com <hambining@HamBook.local>
2024-07-30 04:11:02 +0900

    Modify Class GameResult

--- GameResult.java
+++ GameResult.java
@@ -1,19 +1,19 @@
 public class GameResult {
+    UserInput userInput = new UserInput();
 
-    int again = Result.again;
-
-    public void getGameResult(int strike, int ball) {
+    public void getGameResult(int strike, int ball, ComNumList comNumList, UserNumList userNumList) {
         if (strike == 0 && ball == 0) {
             Messages.NOTHING.println();
         } else if (strike == 3) {
             Messages.END_GAME.println();
             Messages.RESTART_GAME.print();
-            // Initialize again
-            again = 0;
+
+            Result.setAgain(userInput.getUserInput());
+            comNumList.clear();
+            userNumList.clear();
         } else {
             Messages.PRINT_BALL.printNum(ball);
             Messages.PRINT_STRIKE.printNum(strike);
-            System.out.println();
         }
     }
 }
