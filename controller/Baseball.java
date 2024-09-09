cc61e5f6cb80970d2f51ffce216d38dee232ebb1
hambining <hyunbini2942@gmail.com>
2024-09-09 22:11:07 +0900

    Change folders

--- controller/Baseball.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-public class Baseball {
-    public static void main(String[] args) {
-        GameSetting gameSetting = new GameSetting();
-        gameSetting.startGame();
-    }
-}
