e0de5805d5e640903f9b9c19ea6d31cbcbc5f00f
hambining <hyunbini2942@gmail.com>
2024-09-30 03:54:25 +0900

    Remove comment

--- race/controller/Setting.java
+++ race/controller/Setting.java
@@ -13,31 +13,25 @@ import java.util.List;
 public class Setting {
 
     public void setRace() {
-        // 사용자에게 입력 받기
         UserInput userInput = new UserInput();
 
         String[] carNames = userInput.getCarNamesArr();
         CheckException checkException = new CheckException();
         checkException.checkIllegalArgumentException(carNames);
         int amount = userInput.getAmount();
-
-
-        // 경주할 자동차 생성
+        
         List<Car> carList = getCars(carNames);
         Cars cars = new Cars(carList);
 
 
-        // 경주 조건 세팅
         CarCondition carCondition = new CarCondition(cars.getCars());
 
-        // 경주 시작 및 결과값 출력
         Messages.RESULT.println();
         for (int i = 0; i < amount; i++) {
             race(carCondition, cars);
             Messages.printProcess(cars.getCars());
         }
 
-        // 우승자 출력
         Winner winner = new Winner(cars.getCars());
         Messages.printResult(winner.getWinnersNames());
     }
