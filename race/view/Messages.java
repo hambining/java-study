6f72ebd2fadaf06469c716e7505f623a59e9d550
hambining <hyunbini2942@gmail.com>
2024-10-11 04:36:38 +0900

    Remove unnecessary methods and statements

--- race/view/Messages.java
+++ race/view/Messages.java
@@ -1,9 +1,5 @@
 package race.view;
 
-import race.model.Car;
-
-import java.util.List;
-
 public enum Messages {
     ENTER_NAME("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"),
     ENTER_AMOUNT("시도할 횟수는 몇회인가요?"),
@@ -24,31 +20,13 @@ public enum Messages {
     public void print() {
         System.out.print(message);
     }
+
     public void println() {
         System.out.println(message);
     }
 
     public void printf(String name) {
         System.out.printf(message, name);
-    }
-
-    public void printFor(int amount) {
-        for (int i = 0; i < amount; i++) {
-            System.out.print(message);
-        }
-        System.out.println();
-    }
-
-    public static void printProcess(List<Car> cars) {
-        cars.forEach(car -> {
-            String name = car.getName();
-            int distance = car.getDistance();
-
-            CAR_NAME.printf(name);
-            SEPARATOR_COLON.print();
-            DISTANCE.printFor(distance);
-        });
-        System.out.println();
     }
 
     public static void printResult(String winnersNames) {
