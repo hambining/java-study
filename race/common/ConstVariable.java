7002ecbf7c5a0a32182e3f684ed047a2e8e6b13c
hambining <hyunbini2942@gmail.com>
2024-10-11 22:01:53 +0900

    Specify the name of constant variable

--- race/common/ConstVariable.java
+++ race/common/ConstVariable.java
@@ -3,7 +3,7 @@ package race.common;
 public class ConstVariable {
     public static final int NAME_LENGTH_LIMIT = 5;
     public static final int FORWARD_CONDITION = 4;
-    public static final int RAND_NUM_BOUND = 10;
+    public static final int RANDOM_NUMBER_BOUND = 10;
     public static final int UNNECESSARY_BLANK = 2;
     public static final String SEPARATOR_COMMA = ", ";
     public static final String SEPARATOR_COLON = " : ";
