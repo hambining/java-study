24cf0047d28911a1f25ad80d50fcd0f0fc5cea5b
hambining <hyunbini2942@gmail.com>
2024-11-14 03:16:45 +0900

    Create class Validation

---
+++ baseball/common/Validation.java
@@ -0,0 +1,11 @@
+package baseball.common;
+
+public class Validation {
+    // userInput 들어오는 숫자가 3자리인지도 검증
+    public static boolean isOverThanMaxInputValue(int num) {
+        return String.valueOf(num).length() > ConstVariable.SIZE_OF_NUMBER_LIST;
+    }
+
+    // 그 뭐냐 userInput 중복 안 되게 검증
+    // 은 조금 어려운데
+}
