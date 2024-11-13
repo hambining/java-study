9756745d2c20f00bffb7aefcb60dd458f8a61e67
hambining <hyunbini2942@gmail.com>
2024-11-13 18:51:58 +0900

    Use ConstVariable instead of magic number

--- baseball/controller/RandomNum.java
+++ baseball/controller/RandomNum.java
@@ -8,7 +8,7 @@ public class RandomNum {
 
     public int[] getRandNumArr() {
         Random random = new Random();
-        int[] randNumArr = new int[3];
+        int[] randNumArr = new int[ConstVariable.SIZE_OF_NUMBER_LIST];
         for (int i : randNumArr) {
             randNumArr[i] = random.nextInt(9) + 1;
         }
