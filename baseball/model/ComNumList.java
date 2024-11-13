90d5d72383ce0f0ec95156ce28c710c0ed327355
hambining <hyunbini2942@gmail.com>
2024-11-14 02:29:10 +0900

    Edit ComNumList&UserNumList extend NumList

--- baseball/model/ComNumList.java
+++ baseball/model/ComNumList.java
@@ -1,39 +1,17 @@
 package baseball.model;
 import baseball.common.ConstVariable;
-import baseball.controller.RandomNum;
 
 import java.util.ArrayList;
 
-public class ComNumList implements NumList {
+public class ComNumList extends NumList {
     ArrayList<Integer> comNumList;
 
     public ComNumList() {
         comNumList = new ArrayList<>(ConstVariable.SIZE_OF_NUMBER_LIST);
     }
 
-    public void setComNumList(RandomNum randomNum) {
-        // Add 3 random numbers to comNumList
-        while (comNumList.size() < ConstVariable.SIZE_OF_NUMBER_LIST) {
-            addNum(randomNum.getRandomNum());
-        }
-        System.out.println();
-    }
-
-    @Override
-    public void addNum(int num) {
-        if (!comNumList.contains(num)) {
-            comNumList.add(num);
-            System.out.print(num);
-        }
-    }
-
     @Override
     public void clear() {
-        comNumList.clear();
-    }
-
-    @Override
-    public Integer getNum(int index) {
-        return comNumList.get(index);
+        super.clear();
     }
 }
--- baseball/model/UserNumList.java
+++ baseball/model/UserNumList.java
@@ -1,37 +1,18 @@
 package baseball.model;
 
 import baseball.common.ConstVariable;
-import baseball.view.UserInput;
-import baseball.view.Messages;
 
 import java.util.ArrayList;
 
-public class UserNumList implements NumList {
+public class UserNumList extends NumList {
     ArrayList<Integer> userNumList;
+
     public UserNumList() {
         userNumList = new ArrayList<>(ConstVariable.SIZE_OF_NUMBER_LIST);
     }
 
-    public void setUserNumList(UserInput input) {
-        Messages.INPUT_NUMBER.print();
-        addNum(input.getUserInput());
-    }
-
-    @Override
-    public void addNum(int num) {
-        while (num > 0) {
-            userNumList.add(0, num % 10);
-            num /= 10;
-        }
-    }
-
     @Override
     public void clear() {
-        userNumList.clear();
-    }
-
-    @Override
-    public Integer getNum(int index) {
-        return userNumList.get(index);
+        super.clear();
     }
 }
