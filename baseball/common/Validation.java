346fb44abc8c027712c22a1be865141551339641
hambining <hyunbini2942@gmail.com>
2024-11-19 23:03:26 +0900

    Check userInput has duplicated numbers or not

--- baseball/common/Validation.java
+++ baseball/common/Validation.java
@@ -1,5 +1,7 @@
 package baseball.common;
 
+import java.util.Arrays;
+
 public class Validation {
     // userInput 들어오는 숫자가 3자리인지도 검증
     public static boolean isOverThanMaxInputLength(int num) {
@@ -8,4 +10,7 @@ public class Validation {
 
     // 그 뭐냐 userInput 중복 안 되게 검증
     // 은 조금 어려운데
+    public static boolean hasDuplicationNumbers(int[] numArr) {
+        return Arrays.stream(numArr).distinct().count() != numArr.length;
+    }
 }
