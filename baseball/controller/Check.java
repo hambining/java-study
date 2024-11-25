67df6135bb0f750c68951cbac1022c1aae11486c
hambining <hyunbini2942@gmail.com>
2024-11-25 18:42:06 +0900

    Reduce depth of counting ball and strike

--- baseball/controller/Check.java
+++ baseball/controller/Check.java
@@ -6,16 +6,32 @@ import baseball.model.Result;
 
 public class Check {
     public void count(NumList comNumList, NumList userNumList, Result result) {
+        countStrike(comNumList, userNumList, result);
+        countBall(comNumList, userNumList, result);
+        removeDuplicatedBallCount(result);
+    }
+
+    public void countStrike(NumList comNumList, NumList userNumList, Result result) {
+        for (int i = 0; i < ConstVariable.SIZE_OF_NUMBER_LIST; i++) {
+            if (comNumList.getNum(i).equals(userNumList.getNum(i))) {
+                result.increaseStrike();
+            }
+        }
+    }
+
+    public void countBall(NumList comNumList, NumList userNumList, Result result) {
         for (int i = 0; i < ConstVariable.SIZE_OF_NUMBER_LIST; i++) {
             for (int j = 0; j < ConstVariable.SIZE_OF_NUMBER_LIST; j++) {
                 if ((comNumList.getNum(i)).equals(userNumList.getNum(j))) {
                     result.increaseBall();
-                    if (i == j) {
-                        result.decreaseBall();
-                        result.increaseStrike();
-                    }
                 }
             }
+        }
+    }
+
+    public void removeDuplicatedBallCount(Result result) {
+        for (int i = 0; i < result.getStrike(); i++) {
+            result.decreaseBall();
         }
     }
 
