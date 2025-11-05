cd415eb30104b3a526852f4ca322ac61ac6ff912
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-11-05 10:48:30 +0900

    feat: add Constants and Validation utilities

---
+++ lotto/src/main/java/lotto/common/Constants.java
@@ -0,0 +1,11 @@
+package lotto.common;
+
+public class Constants {
+    public static final String SEPARATOR = ",";
+    public static final int TICKET_PRICE = 1000;
+    public static final int LOTTO_BOUND_START = 1;
+    public static final int LOTTO_BOUND_END = 45;
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
+}
